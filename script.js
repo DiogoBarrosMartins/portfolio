45aa0fd681b21ddf14792ece00ef0003e977ee16
Diogo Martins <diogobarrosmartins@hotmail.com>
2024-06-11 19:17:43 +0100

    update projects

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const projectsContainer = document.querySelector('.projects-container');
+    const projectItems = document.querySelectorAll('.project-item');
+
+    const adjustProjectItemWidths = () => {
+        const numberOfProjects = projectItems.length;
+        projectItems.forEach(item => {
+            item.style.flex = `1 1 calc(100% / ${numberOfProjects} - 1rem)`;
+            item.style.maxWidth = `calc(100% / ${numberOfProjects} - 1rem)`;
+        });
+    };
+
+    adjustProjectItemWidths();
+    window.addEventListener('resize', adjustProjectItemWidths);
+});
